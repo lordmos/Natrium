bf272c64134f50135c5eaca9f0c12e75781763cf
Hui Zhong <xanogayu@live.com>
2018-05-18 18:34:30 +0800

    uninstall hook

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,3 @@
-#! /usr/bin/env node
-
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const projectProfile = "project.profile.json";
---
+++ dist/post-uninstall.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = __importStar(require("path"));
+const fs = __importStar(require("fs"));
+const rootPath = path.join(__dirname, "..", "..", "..");
+const packageJsonPath = path.join(rootPath, "package.json");
+const ngCliJsonPath = path.join(rootPath, ".angular-cli.json");
+console.log(ngCliJsonPath);
+fs.exists(ngCliJsonPath, (exists) => {
+    if (!exists) {
+        console.error("Natrium : .angular-cli.json file is not exist , are your sure this is an angular project ?");
+    }
+    else {
+        fs.readFile(packageJsonPath, "utf8", (err, file) => {
+            if (err) {
+                console.log("Natrium : " + err.message);
+                console.error("Natrium : package.json file is not exist , are your sure this is an angular project ?");
+            }
+            else {
+                const config = JSON.parse(file.toString());
+                delete config.scripts["natrium"];
+                fs.writeFileSync(packageJsonPath, JSON.stringify(config, undefined, 2));
+                console.log("Natrium uninstall configuration is completed . Good bye !");
+            }
+        });
+    }
+});
+//# sourceMappingURL=post-uninstall.js.map
